753359d5bef14ed1ef4faf3241ce8c52f4a981da
Mitchell Nicholls <29205444@students.lincoln.ac.uk>
2026-04-28 16:24:06 +0100

    Basic Fan Rotation
    Fan objects spin with coroutine. Flexible delay for game balancing.

---
+++ Assets/Scripts/FanManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using UnityEngine;
+
+public class FanManager : MonoBehaviour
+{
+    public float rotationSpeed = 0f;
+
+    void Start()
+    {
+        StartCoroutine(FanRoutine());
+    }
+
+    void Update()
+    {
+        // Always rotate using current speed
+        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+    }
+
+    IEnumerator FanRoutine()
+    {
+        while (true)
+        {
+            // Start fan
+            rotationSpeed = 2000f;
+            yield return new WaitForSeconds(5f);
+
+            // Stop fan
+            rotationSpeed = 0f;
+            yield return new WaitForSeconds(5f);
+        }
+    }
+}
